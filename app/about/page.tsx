ea668b1f5fdd543bda4738a62f191adc9be57a25
dangquynhnguyen <nguyendangquynh94@gmail.com>
2025-02-04 23:15:27 +0100

    Add 'About' page

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+export interface AboutPageProps {}
+
+export default function AboutPage(props: AboutPageProps) {
+	return <div>About Page</div>
+}
