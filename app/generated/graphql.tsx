5bfbca087d855ac3d652073457e688f6a650911e
dangquynhnguyen <nguyendangquynh94@gmail.com>
2025-02-07 12:31:53 +0100

    install new package @graphql-codegen
    - change mutations.ts to mutations.graphql
    - refactoring : page Register

---
+++ app/generated/graphql.tsx
@@ -0,0 +1,154 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as Apollo from '@apollo/client'
+import { gql } from '@apollo/client'
+export type Maybe<T> = T | null
+export type InputMaybe<T> = Maybe<T>
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] }
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> }
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> }
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = {
+	[_ in K]?: never
+}
+export type Incremental<T> =
+	| T
+	| { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never }
+const defaultOptions = {} as const
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+	ID: { input: string; output: string }
+	String: { input: string; output: string }
+	Boolean: { input: boolean; output: boolean }
+	Int: { input: number; output: number }
+	Float: { input: number; output: number }
+	DateTimeISO: { input: any; output: any }
+}
+
+export type FieldError = {
+	__typename?: 'FieldError'
+	field: Scalars['String']['output']
+	message: Scalars['String']['output']
+}
+
+export type IMutationResponse = {
+	code: Scalars['Float']['output']
+	message?: Maybe<Scalars['String']['output']>
+	success: Scalars['Boolean']['output']
+}
+
+export type LoginInput = {
+	password: Scalars['String']['input']
+	usernameOrEmail: Scalars['String']['input']
+}
+
+export type Mutation = {
+	__typename?: 'Mutation'
+	login: UserMutationResponse
+	logout: Scalars['Boolean']['output']
+	register?: Maybe<UserMutationResponse>
+}
+
+export type MutationLoginArgs = {
+	loginInput: LoginInput
+}
+
+export type MutationRegisterArgs = {
+	registerInput: RegisterInput
+}
+
+export type Query = {
+	__typename?: 'Query'
+	hello: Scalars['String']['output']
+}
+
+export type RegisterInput = {
+	email: Scalars['String']['input']
+	password: Scalars['String']['input']
+	username: Scalars['String']['input']
+}
+
+export type User = {
+	__typename?: 'User'
+	createdAt: Scalars['DateTimeISO']['output']
+	email: Scalars['String']['output']
+	id: Scalars['ID']['output']
+	updatedAt: Scalars['DateTimeISO']['output']
+	username: Scalars['String']['output']
+}
+
+export type UserMutationResponse = IMutationResponse & {
+	__typename?: 'UserMutationResponse'
+	code: Scalars['Float']['output']
+	errors?: Maybe<Array<FieldError>>
+	message?: Maybe<Scalars['String']['output']>
+	success: Scalars['Boolean']['output']
+	user?: Maybe<User>
+}
+
+export type RegisterMutationVariables = Exact<{
+	registerInput: RegisterInput
+}>
+
+export type RegisterMutation = {
+	__typename?: 'Mutation'
+	register?: {
+		__typename?: 'UserMutationResponse'
+		code: number
+		success: boolean
+		message?: string | null
+		user?: { __typename?: 'User'; id: string; username: string; email: string } | null
+		errors?: Array<{ __typename?: 'FieldError'; field: string; message: string }> | null
+	} | null
+}
+
+export const RegisterDocument = gql`
+	mutation Register($registerInput: RegisterInput!) {
+		register(registerInput: $registerInput) {
+			code
+			success
+			message
+			user {
+				id
+				username
+				email
+			}
+			errors {
+				field
+				message
+			}
+		}
+	}
+`
+export type RegisterMutationFn = Apollo.MutationFunction<
+	RegisterMutation,
+	RegisterMutationVariables
+>
+
+/**
+ * __useRegisterMutation__
+ *
+ * To run a mutation, you first call `useRegisterMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useRegisterMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [registerMutation, { data, loading, error }] = useRegisterMutation({
+ *   variables: {
+ *      registerInput: // value for 'registerInput'
+ *   },
+ * });
+ */
+export function useRegisterMutation(
+	baseOptions?: Apollo.MutationHookOptions<RegisterMutation, RegisterMutationVariables>,
+) {
+	const options = { ...defaultOptions, ...baseOptions }
+	return Apollo.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument, options)
+}
+export type RegisterMutationHookResult = ReturnType<typeof useRegisterMutation>
+export type RegisterMutationResult = Apollo.MutationResult<RegisterMutation>
+export type RegisterMutationOptions = Apollo.BaseMutationOptions<
+	RegisterMutation,
+	RegisterMutationVariables
+>
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,43 +4,27 @@ import { Box, Button } from '@mui/material'
 import { Form, Formik } from 'formik'
 import InputField from '../../components/InputField'
 import Wrapper from '../../components/Wrapper'
-
-import { useMutation } from '@apollo/client'
-import { registerMutation } from '../graphql_client/mutations'
+import { RegisterInput, useRegisterMutation } from '../generated/graphql'
 
 export default function Register() {
-	const initialValues: NewUserInput = { username: '', email: '', password: '' }
+	const initialValues: RegisterInput = { username: '', email: '', password: '' }
 
-	interface UserMutationResponse {
-		code: number
-		success: boolean
-		message: string
-		user: string
-		error: string
-	}
-	interface NewUserInput {
-		username: string
-		email: string
-		password: string
-	}
+	const [registerUser, { data, error }] = useRegisterMutation()
 
-	const [registerUser, { data, error }] = useMutation<
-		{ register: UserMutationResponse },
-		{ registerInput: NewUserInput }
-	>(registerMutation)
-
-	const onRegisterSubmit = (values: NewUserInput) => {
-		registerUser({
+	const onRegisterSubmit = async (values: RegisterInput) => {
+		const response = await registerUser({
 			variables: {
 				registerInput: values,
 			},
 		})
+
+		console.log('RESPONSE', response)
 	}
 
 	return (
 		<Wrapper>
 			{error && <p>Failed to register</p>}
-			{data && data.register.success && <p>Registered successfully{JSON.stringify(data)}</p>}
+			{data && data.register?.success && <p>Registered successfully{JSON.stringify(data)}</p>}
 			<Formik initialValues={initialValues} onSubmit={onRegisterSubmit}>
 				{({ isSubmitting }) => (
 					<Form>
