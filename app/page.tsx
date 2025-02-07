1c0c7894ec7d7f5d5887d22b511390fcd2e04fe1
dangquynhnguyen <nguyendangquynh94@gmail.com>
2025-02-07 13:39:20 +0100

    refactoring :
    - new helper mapFieldErrors
    - show error in register page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
 'use client'
 
 import Box from '@mui/material/Box'
-import Register from './register/page'
 
 export default function Home() {
-	return (
-		<Box>
-			<Register />
-		</Box>
-	)
+	return <Box>Home</Box>
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,24 +1,35 @@
 'use client'
 
 import { Box, Button } from '@mui/material'
-import { Form, Formik } from 'formik'
+import { Form, Formik, FormikHelpers } from 'formik'
+import { useRouter } from 'next/navigation'
 import InputField from '../../components/InputField'
 import Wrapper from '../../components/Wrapper'
 import { RegisterInput, useRegisterMutation } from '../generated/graphql'
+import { mapFieldErrors } from '../helpers/mapFieldErrors'
 
 export default function Register() {
 	const initialValues: RegisterInput = { username: '', email: '', password: '' }
+	const router = useRouter()
 
 	const [registerUser, { data, error }] = useRegisterMutation()
 
-	const onRegisterSubmit = async (values: RegisterInput) => {
+	const onRegisterSubmit = async (
+		values: RegisterInput,
+		{ setErrors }: FormikHelpers<RegisterInput>,
+	) => {
 		const response = await registerUser({
 			variables: {
 				registerInput: values,
 			},
 		})
 
-		console.log('RESPONSE', response)
+		if (response.data?.register?.errors) {
+			setErrors(mapFieldErrors(response.data.register.errors))
+		} else if (response.data?.register?.user) {
+			// Register successfully
+			router.push('/')
+		}
 	}
 
 	return (
