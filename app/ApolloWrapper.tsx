e55befb0bda594337cd797d963cb4894cf7fe15f
dangquynhnguyen <nguyendangquynh94@gmail.com>
2025-02-11 21:40:25 +0100

    .

--- app/ApolloWrapper.tsx
+++ app/ApolloWrapper.tsx
@@ -10,7 +10,7 @@ import {
 function makeClient() {
 	const httpLink = new HttpLink({
 		// this needs to be an absolute url, as relative urls cannot be used in SSR
-		uri: 'http://localhost:4000/graphql',
+		uri: 'https://blocdash-nextjs-server.onrender.com/graphql',
 		// you can disable result caching here if you want to
 		// (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
 		fetchOptions: { cache: 'force-cache' },
