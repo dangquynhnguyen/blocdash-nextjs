07d54fd15e4071011ccd7093ce4167097b2fd62f
dangquynhnguyen <nguyendangquynh94@gmail.com>
2025-02-16 23:30:22 +0100

    fix cookie

--- app/ApolloWrapper.tsx
+++ app/ApolloWrapper.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client'
 
-import { HttpLink } from '@apollo/client'
+import { __prod__ } from '@/constants'
 import {
 	ApolloClient,
 	ApolloNextAppProvider,
@@ -8,24 +9,10 @@ import {
 } from '@apollo/experimental-nextjs-app-support'
 
 function makeClient() {
-	const httpLink = new HttpLink({
-		// this needs to be an absolute url, as relative urls cannot be used in SSR
-		uri: 'https://api.blocdash.com/graphql',
-		// you can disable result caching here if you want to
-		// (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
-		fetchOptions: { cache: 'force-cache' },
-		// you can override the default `fetchOptions` on a per query basis
-		// via the `context` property on the options passed as a second argument
-		// to an Apollo Client data fetching hook, e.g.:
-		// const { data } = useSuspenseQuery(MY_QUERY, { context: { fetchOptions: { cache: "force-cache" }}});
-		credentials: 'include',
-	})
-
-	// use the `ApolloClient` from "@apollo/experimental-nextjs-app-support"
 	return new ApolloClient({
-		// use the `InMemoryCache` from "@apollo/experimental-nextjs-app-support"
+		uri: __prod__ ? 'https://api.blocdash.com/graphql' : 'http://localhost:4000/graphql',
 		cache: new InMemoryCache(),
-		link: httpLink,
+		credentials: 'include',
 	})
 }
 
