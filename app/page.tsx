f8e5fe7c67e7e777823604e98ab4fc2c6bfd31db
dangquynhnguyen <nguyendangquynh94@gmail.com>
2025-02-16 21:00:46 +0100

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 'use client'
 
 import Box from '@mui/material/Box'
-import { useHelloQuery } from './generated/graphql'
 
 export default function Home() {
-	const { data } = useHelloQuery()
 	return (
 		<Box>
 			<p>Home</p>
-			{data?.hello}
 		</Box>
 	)
 }
