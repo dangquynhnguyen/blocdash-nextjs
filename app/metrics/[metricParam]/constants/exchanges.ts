017cd1436bf34e6e9e5f5c01be3808ad2e59b1d1
dangquynhnguyen <nguyendangquynh94@gmail.com>
2025-03-12 13:58:57 +0100

    update constants for TreeView

--- app/metrics/[metricParam]/constants/exchanges.ts
+++ app/metrics/[metricParam]/constants/exchanges.ts
@@ -7,17 +7,11 @@ export const exchanges: Category = {
 	isAvailable: true,
 	children: [
 		{
-			id: 'exchange-activity',
-			label: 'Exchange Activity',
+			id: 'exchange-flows',
+			label: 'Exchange Flows',
 			type: 'group',
 			isAvailable: true,
 			children: [
-				{
-					id: 'exchange-netflow-volume',
-					label: 'Exchange Netflow Volume',
-					type: 'indicator',
-					isAvailable: true,
-				},
 				{
 					id: 'exchange-inflow-volume',
 					label: 'Exchange Inflow Volume',
@@ -27,6 +21,120 @@ export const exchanges: Category = {
 				{
 					id: 'exchange-outflow-volume',
 					label: 'Exchange Outflow Volume',
+					type: 'indicator',
+					isAvailable: true,
+				},
+				{
+					id: 'exchange-netflow-volume',
+					label: 'Exchange Netflow Volume',
+					type: 'indicator',
+					isAvailable: true,
+				},
+				{
+					id: 'exchange-deposits-transfersCount',
+					label: 'Exchange Deposits: Number of Transfers',
+					type: 'indicator',
+					isAvailable: true,
+				},
+				{
+					id: 'exchange-withdrawals-transfersCount',
+					label: 'Exchange Withdrawals: Number of Transfers',
+					type: 'indicator',
+					isAvailable: true,
+				},
+				{
+					id: 'inHouse-exchange-volume',
+					label: 'In-House Exchange Volume',
+					type: 'indicator',
+					isAvailable: true,
+				},
+				{
+					id: 'interExchange-volume',
+					label: 'Inter-Exchange Volume',
+					type: 'indicator',
+					isAvailable: true,
+				},
+				{
+					id: 'interExchange-transfersCount',
+					label: 'Inter-Exchange: Number of Transfers',
+					type: 'indicator',
+					isAvailable: true,
+				},
+				{
+					id: 'exchange-totalVolume',
+					label: 'Exchange Deposit and Withdrawal Volume (Total)',
+					type: 'indicator',
+					isAvailable: true,
+				},
+				{
+					id: 'exchange-totalVolume-momentum',
+					label: 'Exchange Volume Momentum',
+					type: 'indicator',
+					isAvailable: true,
+				},
+			],
+		},
+		{
+			id: 'exchange-balances',
+			label: 'Exchange Balances',
+			type: 'group',
+			isAvailable: true,
+			children: [
+				{
+					id: 'exchange-balance-total',
+					label: 'Exchange Balance (Total)',
+					type: 'indicator',
+					isAvailable: true,
+				},
+				{
+					id: 'exchange-balance-percent',
+					label: 'Exchange Balance (Percent)',
+					type: 'indicator',
+					isAvailable: true,
+				},
+				{
+					id: 'exchange-net-positionChange',
+					label: 'Exchange Net Position Change',
+					type: 'indicator',
+					isAvailable: true,
+				},
+			],
+		},
+		{
+			id: 'exchange-dominance',
+			label: 'Exchange Dominance',
+			type: 'group',
+			isAvailable: true,
+			children: [
+				{
+					id: 'exchange-transaction-dominance',
+					label: 'Exchange Transaction Dominance',
+					type: 'indicator',
+					isAvailable: true,
+				},
+				{
+					id: 'exchange-volume-dominance',
+					label: 'Exchange Volume Dominance',
+					type: 'indicator',
+					isAvailable: true,
+				},
+			],
+		},
+		{
+			id: 'exchange-prices',
+			label: 'Exchange Prices',
+			type: 'group',
+			isAvailable: true,
+			children: [
+				{
+					id: 'exchange-withdrawal-price',
+					label: 'Exchange Withdrawal Price',
+					type: 'indicator',
+					isAvailable: true,
+				},
+				{
+					id: 'exchange-deposit-price',
+					label: 'Exchange Deposit Price',
 					type: 'indicator',
 					isAvailable: true,
 				},
--- app/metrics/[metricParam]/constants/profitLoss.ts
+++ app/metrics/[metricParam]/constants/profitLoss.ts
@@ -4,37 +4,99 @@ export const profitLoss: Category = {
 	id: 'profit-loss',
 	label: 'Profit/Loss',
 	type: 'category',
-	isAvailable: false,
+	isAvailable: true,
 	children: [
-		// {
-		//   id: '4.1',
-		//   label: '...',
-		//   type: 'indicator',
-		//   isAvailable: true,
-		// },
-		// {
-		//   id: '4.2',
-		//   label: '...',
-		//   type: 'indicator',
-		//   isAvailable: true,
-		// },
-		// {
-		//   id: '4.3',
-		//   label: '...',
-		//   type: 'indicator',
-		//   isAvailable: true,
-		// },
-		// {
-		//   id: '4.4',
-		//   label: '...',
-		//   type: 'indicator',
-		//   isAvailable: true,
-		// },
-		// {
-		//   id: '4.5',
-		//   label: '...',
-		//   type: 'indicator',
-		//   isAvailable: true,
-		// },
+		{
+			id: 'group-sopr',
+			label: 'SOPR - Spent Output Profit Ratio',
+			type: 'group',
+			isAvailable: true,
+			children: [
+				{
+					id: 'sopr',
+					label: 'SOPR',
+					type: 'indicator',
+					isAvailable: true,
+				},
+			],
+		},
+		{
+			id: 'supply-profitLoss',
+			label: 'Supply in Profit/Loss',
+			type: 'group',
+			isAvailable: true,
+			children: [
+				{
+					id: 'supply-profit-percent',
+					label: 'Percent of Supply in Profit',
+					type: 'indicator',
+					isAvailable: true,
+				},
+				{
+					id: 'supply-profit',
+					label: 'Supply in Profit',
+					type: 'indicator',
+					isAvailable: true,
+				},
+				{
+					id: 'supply-loss',
+					label: 'Supply in Loss',
+					type: 'indicator',
+					isAvailable: true,
+				},
+			],
+		},
+		{
+			id: 'unrealized-profitLoss',
+			label: 'Unrealized Profit/Loss',
+			type: 'group',
+			isAvailable: true,
+			children: [
+				{
+					id: 'nupl',
+					label: 'NUPL - Net Unrealized Profit/Loss',
+					type: 'indicator',
+					isAvailable: true,
+				},
+				{
+					id: 'relative-unrealized-profit',
+					label: 'Relative Unrealized Profit',
+					type: 'indicator',
+					isAvailable: true,
+				},
+				{
+					id: 'relative-unrealized-loss',
+					label: 'Relative Unrealized Loss',
+					type: 'indicator',
+					isAvailable: true,
+				},
+			],
+		},
+		{
+			id: 'realized-profitLoss',
+			label: 'Realized Profit/Loss',
+			type: 'group',
+			isAvailable: true,
+			children: [
+				{
+					id: 'net-realized-profitLoss',
+					label: 'Net Realized Profit/Loss',
+					type: 'indicator',
+					isAvailable: true,
+				},
+				{
+					id: 'realized-profit',
+					label: 'Realized Profit',
+					type: 'indicator',
+					isAvailable: true,
+				},
+				{
+					id: 'realized-loss',
+					label: 'Realized Loss',
+					type: 'indicator',
+					isAvailable: true,
+				},
+			],
+		},
 	],
 }
